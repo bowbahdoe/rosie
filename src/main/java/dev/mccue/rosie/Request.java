1d3f0642765ec0ad0ef0381b80283fc4d6ca5563
Ethan McCue <ethan@mccue.dev>
2022-09-06 20:26:47 -0400

    ...

--- src/main/java/dev/mccue/rosie/Request.java
+++ src/main/java/dev/mccue/rosie/Request.java
@@ -1,6 +1,5 @@
 package dev.mccue.rosie;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.InputStream;
 import java.security.cert.X509Certificate;
 import java.util.Map;
@@ -12,19 +11,10 @@ import java.util.Optional;
  * <a href="https://github.com/ring-clojure/ring/blob/1.9.0/ring-servlet/src/ring/util/servlet.clj#L37">...</a>
  */
 public interface Request {
-    /**
-     * Analogous to {@link HttpServletRequest#getServerPort()}.
-     */
     int serverPort();
 
-    /**
-     * Analogous to {@link HttpServletRequest#getServerName()}.
-     */
     String serverName();
 
-    /**
-     * Analogous to {@link HttpServletRequest#getRemoteAddr()}.
-     */
     String remoteAddr();
 
     String uri();
@@ -33,14 +23,8 @@ public interface Request {
 
     String scheme();
 
-    /**
-     * Analogous to {@link HttpServletRequest#getMethod()}, but will always be lowercase.
-     */
     String requestMethod();
 
-    /**
-     * Analogous to {@link HttpServletRequest#getProtocol()}.
-     */
     String protocol();
 
     /**
@@ -51,19 +35,19 @@ public interface Request {
     Map<String, String> headers();
 
     /**
-     * Analogous to {@link HttpServletRequest#getContentType()}, but will be an empty Optional
+     * Will be an empty Optional
      * if the content length was not given in the request.
      */
     Optional<String> contentType();
 
     /**
-     * Analogous to {@link HttpServletRequest#getContentLength()}, but will be an empty Optional
+     * Will be an empty Optional
      * if the content length was reported as negative.
      */
     Optional<Integer> contentLength();
 
     /**
-     * Analogous to {@link HttpServletRequest#getCharacterEncoding()}, but will be an empty Optional
+     * Will be an empty Optional
      * if the character encoding was not given in the request.
      */
     Optional<String> characterEncoding();
@@ -73,9 +57,6 @@ public interface Request {
      */
     Optional<X509Certificate> sslClientCert();
 
-    /**
-     * Analogous to {@link HttpServletRequest#getInputStream()}.
-     */
     InputStream body();
 }
 
